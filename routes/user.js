8efae62d424339a878573934dc8c5c08532d5a05
Arie <arie@wego.com>
2013-09-09 21:31:47 +0700

    update drm

--- routes/user.js
+++ routes/user.js
@@ -3,19 +3,26 @@
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
-};
+ exports.list = function(req, res){
+ 	res.send("respond with a resource");
+ };
 
-exports.download = function(req, res){
-  res.writeHead(200, {"Content-Type": "application/json"});
-  var otherArray = ["item1", "item2"];
-  var otherObject = { file: "nodejs.drm", url: "/data/download/drm/nodejs.drm",drm : true };
-  res.write(
-    JSON.stringify({ 
-      fileDownload: otherObject, 
-      valid: new Date(),
-    })
-  );
-  res.end();
-};
+ exports.download = function(req, res){
+ 	console.log(req.query.id);
+ 	res.writeHead(200, {"Content-Type": "application/json"});
+ 	var otherObject = { 
+ 		file: "344-221.drm",
+ 		title: 'A.G.N.E.Z',
+ 		url: "/drm/344-221.drm",
+ 		drm : true,
+ 		type : 'DRM',
+ 		valid: new Date() 
+ 	};
+ 	res.write(
+ 		JSON.stringify({
+ 				details: otherObject
+ 				
+ 		})
+ 		);
+ 	res.end();
+ };
